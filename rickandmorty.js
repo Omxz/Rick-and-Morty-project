76b154b9d1440122b238fb80d463a8ecba0bf8cd
Sebastian Svensson <sebastian.j.svensson@gmail.com>
2022-09-09 10:07:27 +0200

    added some fetches to 3 apis

--- rickandmorty.js
+++ rickandmorty.js
@@ -1,17 +1,30 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   
-  function fetchRnM() {
-    fetch("https://rickandmortyapi.com/api", {
-      headers: {
-        Accept: "application/json",
-      },
-    })
+  function fetchCharacters() {
+    fetch("https://rickandmortyapi.com/api/character")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-       
+        console.log(data.results[0])
+      });
+  }
+  function fetchEpisodes() {
+    fetch("https://rickandmortyapi.com/api/episode")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results[0])
+      });
+  }
+  function fetchLocations() {
+    fetch("https://rickandmortyapi.com/api/location")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results[0])
       });
   }
 
-  fetchRnM()
+  fetchCharacters()
+  fetchEpisodes()
+  fetchLocations()
+
+
 });
