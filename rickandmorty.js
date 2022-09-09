260818440dfbdc46fde65a11a56f6b51548f3fb5
Sebastian Svensson <sebastian.j.svensson@gmail.com>
2022-09-09 09:41:08 +0200

    added sass

--- rickandmorty.js
+++ rickandmorty.js
@@ -1,24 +1,17 @@
-document.addEventListener('DOMContentLoaded', (event) => {  
-
+document.addEventListener("DOMContentLoaded", (event) => {
   
-  fetch("https://rickandmortyapi.com/api")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  function fetchRnM() {
+    fetch("https://rickandmortyapi.com/api", {
+      headers: {
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        getImg.src = data.img;
+        getH1.innerHTML = data.title;
+        getImg.alt = data.alt;
+      });
+  }
 });
-
-
-
