9367f147a038379c3310a0925f08700a52400fe7
Sebastian Svensson <sebastian.j.svensson@gmail.com>
2022-09-09 09:45:32 +0200

    Update rickandmorty.js

--- rickandmorty.js
+++ rickandmorty.js
@@ -9,9 +9,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        getImg.src = data.img;
-        getH1.innerHTML = data.title;
-        getImg.alt = data.alt;
+       
       });
   }
+
+  fetchRnM()
 });
