7f52b7337ef7f0003bc600298cdf204e4fdeb9f5
Sebastian Svensson <sebastian.j.svensson@gmail.com>
2022-09-12 10:54:20 +0200

    working speech

--- rickandmorty.js
+++ rickandmorty.js
@@ -1,10 +1,40 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  let buttonChar = document.querySelector(".search-char");
+
+// SPEECH START
+
+const btn = document.querySelector('.talk');
+const content = document.querySelector('.content');
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.onstart = function() {
+  console.log('voice is activated, you can use microphone');
+}
+
+recognition.onresult = function(event) {
+const current = event.resultIndex;
+
+const transcript = event.results[current][0].transcript;
+content.textContent = transcript;
+let a = transcript
+fetchCharacters(a)
+}
+
+// add the listener to the btn
+
+btn.addEventListener('click', () => {
+  recognition.start();
+});
+
+//SPEECH END
+
+
   let cards = document.querySelector(".cards");
 
-  let input = "Morty";
+ 
 
-  function fetchCharacters() {
+  function fetchCharacters(input) {
     let b = 1;
     const maxCount = 6;
     for (let j = 1; j <= 42; j++) {
@@ -65,5 +95,5 @@ document.addEventListener("DOMContentLoaded", (event) => {
   }
   
 
-  fetchCharacters();
+  
 });
